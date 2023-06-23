4fb07bc1bc30fc7a8dfab4b4d83d87b7444cdc01
Damir Sagalbaev <105151275+s1lda@users.noreply.github.com>
2023-06-23 21:31:37 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -10,23 +10,24 @@ public class SquareEquation
         }
         double x1;
         double x2;
-        double[] array = new double[2];
+        double[] array1 = {};
+        double[] array2 = new double[1];
+        double[] array3 = new double[2];
         double d = b * b - 4*a* c;
         if (d <= -double.Epsilon)
         {
-            array = new double[0];
+            return array1
         }
         if (-double.Epsilon < d && d < double.Epsilon)
         {
-             x1 =-b/2*a;
-             array = new double[] { x1 };
+             array2[0]=-b/2*a;
+             return array2
         }
         if (d>0)
         {
-            x1 = -(b+Math.Sign(b)*Math.Sqrt(d))/2;
-            x2 = c / x1;
-            array = new double[] { x1, x2 };
+            array3[0] = -(b+Math.Sign(b)*Math.Sqrt(d))/2;
+            array3[1] = c / array3[0];
+            return array3
         }
-        return array;
     }
 }
