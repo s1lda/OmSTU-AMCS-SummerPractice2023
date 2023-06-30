e60c6342e666940ab571e6b7d8ca1e95f433ed14
Damir Sagalbaev <105151275+s1lda@users.noreply.github.com>
2023-06-30 12:01:57 +0600

    Update UnitTest1.cs

--- BDD.Tests/UnitTest1.cs
+++ BDD.Tests/UnitTest1.cs
@@ -4,69 +4,76 @@ using TechTalk.SpecFlow;
 using System;
 
 [Binding]
-public class SquareEquationSteps
-{
-    private double[] coefficients = new double[3];
-    private double[]? roots;
-    private Exception exception = new();
-    private double precision = 0.001;
-
-
-
-
-    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-    public void GivenCofficients(string p0, string p1, string p2)
+    public class SquareEquationSteps
     {
-        double _p0 = double.Parse(p0, System.Globalization.CultureInfo.InvariantCulture);
-        double _p1 = double.Parse(p1, System.Globalization.CultureInfo.InvariantCulture);
-        double _p2 = double.Parse(p2, System.Globalization.CultureInfo.InvariantCulture);
+        private double[] coefficients = new double[3];
+        private double[]? roots;
+        private Exception exception = new();
+        private double precision = 0.001;
 
-        coefficients = new double[] { _p0, _p1, _p2 };
-    }
 
-    [When(@"вычисляются корни квадратного уравнения")]
-    public void WhenSolveRoots()
-    {
-        try
+       
+
+        [Given(@"РљРІР°РґСЂР°С‚РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ СЃ РєРѕСЌС„С„РёС†РёРµРЅС‚Р°РјРё \((.*), (.*), (.*)\)")]
+        public void GivenCofficients(string p0, string p1, string p2)
         {
-            roots = new double[3];
-            roots = SquareEquation.Solve(coefficients[0], coefficients[1], coefficients[2]);
+            double _p0 = double.Parse(p0, System.Globalization.CultureInfo.InvariantCulture);
+            double _p1 = double.Parse(p1, System.Globalization.CultureInfo.InvariantCulture);
+            double _p2 = double.Parse(p2, System.Globalization.CultureInfo.InvariantCulture);
+
+            coefficients = new double[] { _p0, _p1, _p2 };
         }
-        catch (ArgumentException ex)
+
+        [When(@"РІС‹С‡РёСЃР»СЏСЋС‚СЃСЏ РєРѕСЂРЅРё РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ")]
+        public void WhenSolveRoots()
         {
-            exception = ex;
+            try
+            {
+                roots = new double[3];
+                roots = SquareEquation.Solve(coefficients[0], coefficients[1], coefficients[2]);
+            }
+            catch (ArgumentException ex)
+            {
+                exception = ex;
+            }
         }
-    }
 
-    [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
-    public void ThenTwoRoots(double root1, double root2)
-    {
-        if (root1 == roots[0] && root2 == roots[1])
+        [Then(@"РєРІР°РґСЂР°С‚РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ РґРІР° РєРѕСЂРЅСЏ \((.*), (.*)\) РєСЂР°С‚РЅРѕСЃС‚Рё РѕРґРёРЅ")]
+        public void ThenTwoRoots(double root1, double root2)
         {
-            Assert.Equal(root1, roots[0], precision);
-            Assert.Equal(root2, roots[1], precision);
+            if (root1 == roots[0] && root2 == roots[1])
+            {
+                Assert.Equal(root1, roots[0], precision);
+                Assert.Equal(root2, roots[1], precision);
+            }
+            else
+            {
+                Assert.Equal(root1, roots[1], precision);
+                Assert.Equal(root2, roots[0], precision);
+            }
         }
-        else
+
+        [Then(@"РєРІР°РґСЂР°С‚РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ РѕРґРёРЅ РєРѕСЂРµРЅСЊ (.*) РєСЂР°С‚РЅРѕСЃС‚Рё РґРІР°")]
+        public void ThenOneRoots(double root)
         {
-            Assert.Equal(root1, roots[1], precision);
-            Assert.Equal(root2, roots[0], precision);
+            Assert.Equal(root, roots[0], precision);
         }
+
+        [Then(@"РјРЅРѕР¶РµСЃС‚РІРѕ РєРѕСЂРЅРµР№ РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ РїСѓСЃС‚РѕРµ")]
+        public void ThenZeroRoots()
+        {
+            Assert.Empty(roots);
+        }
+
+        [Then(@"РІС‹Р±СЂР°СЃС‹РІР°РµС‚СЃСЏ РёСЃРєР»СЋС‡РµРЅРёРµ ArgumentException")]
+        public void ThenР’С‹Р±СЂР°СЃС‹РІР°РµС‚СЃСЏРСЃРєР»СЋС‡РµРЅРёРµArgumentException()
+        {
+            Assert.Equal("Invalid Values", exception.Message);
+        }
+
+        
     }
 
-    [Then(@"квадратное уравнение имеет один корень (.*) кратности два")]
-    public void ThenOneRoots(double root)
-    {
-        Assert.Equal(root, roots[0], precision);
-    }
-
-    [Then(@"множество корней квадратного уравнения пустое")]
-    public void ThenZeroRoots()
-    {
-        Assert.Empty(roots);
-    }
-
-    [Then(@"выбрасывается исключение ArgumentException")]
-    public void ThenВыбрасываетсяИсключениеArgumentException()
     {
         Assert.Equal("Invalid Values", exception.Message);
     }
